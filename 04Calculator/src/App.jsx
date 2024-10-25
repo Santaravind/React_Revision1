9d392c55d109ef19debd681ab2c60afc9abd3bbd
Aravind Sant <aravindsant563@gmail.com>
2024-10-25 07:42:28 +0530

    Calculater complit function

--- 04Calculator/src/App.jsx
+++ 04Calculator/src/App.jsx
@@ -3,13 +3,49 @@ import Input from './Component/Input'
 import Buttons from './Component/Buttons'
 
 function App() {
-  
+      const [calVal, setcalVal] = useState("");
+     const ButtonEvent=(butText)=>{
+      if(butText==='C'){
+          setcalVal('')
+      }else if(butText==='='){
+        const result= eval(calVal);
+        setcalVal(result);
+
+      }
+      else if(butText==='del'){
+        const result= calVal.slice(0,-1);
+        setcalVal(result);
+
+      }else if(butText==='!'){
+        const fact= parseInt(calVal,10);
+        if(!isNaN(fact)){
+          const factRsult=factorial(fact);
+          setcalVal(factRsult.toString());
+        }
+
+      }
+      else{
+        const newVal=calVal+butText;
+        setcalVal(newVal);
+      }
+      
+      console.log(butText)}
+
+       const factorial =(n)=>{
+        if(n<0) return 'NaN'
+        if(n===0||n===1)return 1;
+        return n*factorial(n-1);
+
+
+       };
+
   return (
     <>
      <center >
-      <div className=' box-border border-2 w-1/2  '>  
-      <Input/>
-      <Buttons/>
+      <div className=' box-border border-2 w-1/2 mt-3  '> 
+      <h2 className='font-semibold text-2xl bg-purple-300'> Calculater </h2> 
+      <Input  inputVal={calVal}/>
+      <Buttons onButtonclick={ButtonEvent}/>
       </div>
      
      </center>
--- 04Calculator/src/Component/Buttons.jsx
+++ 04Calculator/src/Component/Buttons.jsx
@@ -1,13 +1,19 @@
 import React from 'react'
 
-function Buttons() {
+function Buttons({onButtonclick}) {
     const buttonName=[
-        'C','9','8','7','6','5','4','3','2','1','+','-','/','*','%','del'
-    ]
+        'C','+','-','='
+    ,'9','8','7','*','6','5','4','/','3','2','1','%',
+    '0','^','!','del'
+  ]
   return (
-    <div className='bg-gray-500 pb-2'> 
+    <div className=' pb-2'> 
     <div className='grid grid-cols-4 m-1 mb-2 gap-3 mt-2'>
-        {buttonName.map(col=><button className=' p-1 bg-red-400 rounded-md text-2xl font-bold'>{col}</button>)}
+        {buttonName.map((col)=><button className=' p-1 bg-gray-400 rounded-md text-2xl font-bold'  
+         onClick={()=>onButtonclick(col)} key={col}
+        >{col}</button>)
+        
+        }
       {/* <button className=' p-1 bg-red-400 rounded-md text-2xl font-bold'>C</button>
       <button className=' p-1 bg-red-400 rounded-md text-2xl font-bold'>9</button> 
       <button className=' p-1 bg-red-400 rounded-md text-2xl font-bold'>8</button> 
